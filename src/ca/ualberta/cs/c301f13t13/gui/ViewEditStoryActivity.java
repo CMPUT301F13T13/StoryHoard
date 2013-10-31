d0b3af046bbd5bb45ed4cdbee70690af435e894d
Kim <kwu1@ualberta.ca>
2013-10-31 01:51:54 -0600

    removed redundancies in manifest

---
+++ src/ca/ualberta/cs/c301f13t13/gui/ViewEditStoryActivity.java
@@ -0,0 +1,25 @@
+package ca.ualberta.cs.c301f13t13.gui;
+
+import ca.ualberta.cmput301f13t13.storyhoard.R;
+import ca.ualberta.cmput301f13t13.storyhoard.R.layout;
+import ca.ualberta.cmput301f13t13.storyhoard.R.menu;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ViewEditStoryActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_edit_story);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.view_edit_story, menu);
+		return true;
+	}
+
+}
