92872de0204825256b28c3e45accabafaf920502
Kim <kwu1@ualberta.ca>
2013-10-31 02:27:43 -0600

    added ViewEditChapterActivity

--- src/ca/ualberta/cs/c301f13t13/gui/ViewEditChapterActivity.java
+++ src/ca/ualberta/cs/c301f13t13/gui/ViewEditChapterActivity.java
@@ -7,18 +7,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class ViewEditStoryActivity extends Activity {
+public class ViewEditChapterActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_view_edit_story);
+		setContentView(R.layout.activity_view_edit_chapter);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.view_edit_story, menu);
+		getMenuInflater().inflate(R.menu.view_edit_chapter, menu);
 		return true;
 	}
 
