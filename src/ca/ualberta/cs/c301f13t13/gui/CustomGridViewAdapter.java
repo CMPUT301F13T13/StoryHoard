3f06729300dd4ce92cb3c1bdf7af93e531c310a9
Alexander Wong <admin@alexander-wong.com>
2013-11-01 21:21:12 -0600

    Added thenecessary xml, the custom adapter

---
+++ src/ca/ualberta/cs/c301f13t13/gui/CustomGridViewAdapter.java
@@ -0,0 +1,58 @@
+package ca.ualberta.cs.c301f13t13.gui;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import ca.ualberta.cmput301f13t13.storyhoard.R;
+import ca.ualberta.cs.c301f13t13.backend.Story;
+
+public class CustomGridViewAdapter extends ArrayAdapter<Story> {
+
+	Context context;
+	int layoutResourceId;
+	ArrayList<Story> data = new ArrayList<Story>();
+
+	public CustomGridViewAdapter(Context context, int layoutResourceId,
+			ArrayList<Story> data) {
+		super(context, layoutResourceId, data);
+		this.layoutResourceId = layoutResourceId;
+		this.context = context;
+		this.data = data;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View row = convertView;
+		RecordHolder holder = null;
+
+		if (row == null) {
+			LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+			row = inflater.inflate(layoutResourceId, parent, false);
+
+			holder = new RecordHolder();
+			holder.txtTitle = (TextView) row.findViewById(R.id.item_text);
+			holder.imageItem = (ImageView) row.findViewById(R.id.item_image);
+			row.setTag(holder);
+		} else {
+			holder = (RecordHolder) row.getTag();
+		}
+
+		Story item = data.get(position);
+		holder.txtTitle.setText(item.getTitle());
+		holder.imageItem.setImageBitmap(item.getImage());
+		return row;
+
+	}
+
+	static class RecordHolder {
+		TextView txtTitle;
+		ImageView imageItem;
+	}
+}
