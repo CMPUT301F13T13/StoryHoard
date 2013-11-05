0e29444259a07cc3537f88b61c44b6cb006863fd
Josh Tate <jtate@ualberta.ca>
2013-11-05 09:27:43 -0700

    Started on a "All chapters" activity
    For when an author wants to add a chapter as a choice

---
+++ src/ca/ualberta/cs/c301f13t13/gui/ViewAllChaptersActivity.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ca.ualberta.cs.c301f13t13.gui;
+
+//import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+import ca.ualberta.cmput301f13t13.storyhoard.R;
+
+/**
+ * @author joshuatate
+ *
+ */
+public class ViewAllChaptersActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.activity_all_chapters);
+	}
+
+}
