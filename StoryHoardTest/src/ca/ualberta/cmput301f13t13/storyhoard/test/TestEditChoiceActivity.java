db17b62114a4c5288621d78089dcf820829133b6
Josh Tate <jtate@ualberta.ca>
2013-11-06 11:36:07 -0700

    TestEditChoice cont

--- StoryHoardTest/src/ca/ualberta/cmput301f13t13/storyhoard/test/TestEditChoiceActivity.java
+++ StoryHoardTest/src/ca/ualberta/cmput301f13t13/storyhoard/test/TestEditChoiceActivity.java
@@ -6,15 +6,20 @@ package ca.ualberta.cmput301f13t13.storyhoard.test;
 import org.junit.Before;
 import org.junit.Test;
 
+import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 import ca.ualberta.cs.c301f13t13.gui.EditChoiceActivity;
 
 /**
+ * Test case for the activity to edit choices
+ * 
  * @author joshuatate
  * 
  */
 public class TestEditChoiceActivity extends
 		ActivityInstrumentationTestCase2<EditChoiceActivity> {
+	
+	private Activity mActivity;
 	
 	public TestEditChoiceActivity() {
 		super(EditChoiceActivity.class);
@@ -28,6 +33,8 @@ public class TestEditChoiceActivity extends
 	@Before
 	protected void setUp() throws Exception {
 		super.setUp();
+		mActivity = getActivity();
+		mActivity.findViewById(ca.ualberta.cmput301f13t13.storyhoard.R.layout.activity_edit_choice);
 	}
 
 	@Test
