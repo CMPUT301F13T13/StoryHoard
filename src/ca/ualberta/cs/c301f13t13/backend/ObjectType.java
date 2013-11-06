d9f042cf582d89be70ca160de542c5347bba1c2f
sgil <sgil@ualberta.ca>
2013-11-06 15:10:17 -0700

    forgot to actually add enum class

---
+++ src/ca/ualberta/cs/c301f13t13/backend/ObjectType.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package ca.ualberta.cs.c301f13t13.backend;
+
+/**
+ * @author sgil
+ *
+ */
+public enum ObjectType {
+	CHAPTER, 
+	CHOICE, 
+	MEDIA, 
+	PUBLISHED_STORY, 
+	CACHED_STORY, 
+	CREATED_STORY
+}
