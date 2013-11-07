ca2f84e3073858700fd586ee06b2898f9bdd3acc
Alexander Wong <admin@alexander-wong.com>
2013-11-06 19:36:27 -0700

    Removed unecessary imports

--- src/ca/ualberta/cs/c301f13t13/gui/EditChapterActivity.java
+++ src/ca/ualberta/cs/c301f13t13/gui/EditChapterActivity.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
