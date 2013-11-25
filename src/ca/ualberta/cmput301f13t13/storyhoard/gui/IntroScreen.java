579dfbbd81122affa2be22a42f3e6af9a30c4ae2
Kim <kwu1@ualberta.ca>
2013-11-25 15:56:56 -0700

    added intro screen

---
+++ src/ca/ualberta/cmput301f13t13/storyhoard/gui/IntroScreen.java
@@ -0,0 +1,19 @@
+package ca.ualberta.cmput301f13t13.storyhoard.gui;
+
+import ca.ualberta.cmput301f13t13.storyhoard.R;
+import ca.ualberta.cmput301f13t13.storyhoard.R.layout;
+import ca.ualberta.cmput301f13t13.storyhoard.R.menu;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class IntroScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_intro_screen);
+	}
+
+
+}
