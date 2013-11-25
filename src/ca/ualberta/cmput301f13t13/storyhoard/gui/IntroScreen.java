75bfac175f15de24c0ba115b103f83df89a59a75
Kim <kwu1@ualberta.ca>
2013-11-25 16:37:50 -0700

    made 'invisible' actionbar for loading screen, finished touchups for loading screen

--- src/ca/ualberta/cmput301f13t13/storyhoard/gui/IntroScreen.java
+++ src/ca/ualberta/cmput301f13t13/storyhoard/gui/IntroScreen.java
@@ -1,23 +1,20 @@
 package ca.ualberta.cmput301f13t13.storyhoard.gui;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import ca.ualberta.cmput301f13t13.storyhoard.R;
 
 public class IntroScreen extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_intro_screen);
-		ActionBar actionBar = getActionBar();
-		actionBar.hide();
 		Thread loadingTime = new Thread() {
 			public void run() {
 				try {
-					sleep(3000);
+					sleep(6000);
 					Intent intent = new Intent(IntroScreen.this,
 							ViewBrowseStories.class);
 					startActivity(intent);
