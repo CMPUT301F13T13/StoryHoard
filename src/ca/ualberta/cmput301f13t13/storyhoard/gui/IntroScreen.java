7793f2a523d7421d582ae848ee2f8f1fecab77b8
Kim <kwu1@ualberta.ca>
2013-11-25 16:14:03 -0700

    created thread for IntroScreenActivity

--- src/ca/ualberta/cmput301f13t13/storyhoard/gui/IntroScreen.java
+++ src/ca/ualberta/cmput301f13t13/storyhoard/gui/IntroScreen.java
@@ -1,11 +1,10 @@
 package ca.ualberta.cmput301f13t13.storyhoard.gui;
 
-import ca.ualberta.cmput301f13t13.storyhoard.R;
-import ca.ualberta.cmput301f13t13.storyhoard.R.layout;
-import ca.ualberta.cmput301f13t13.storyhoard.R.menu;
-import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.content.Intent;
+import ca.ualberta.cmput301f13t13.storyhoard.R;
 
 public class IntroScreen extends Activity {
 
@@ -13,7 +12,22 @@ public class IntroScreen extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_intro_screen);
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
+		Thread loadingTime = new Thread() {
+			public void run() {
+				try {
+					sleep(3000);
+					Intent intent = new Intent(IntroScreen.this,
+							ViewBrowseStories.class);
+					startActivity(intent);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					finish();
+				}
+			}
+		};
+		loadingTime.start();
 	}
-
-
 }
