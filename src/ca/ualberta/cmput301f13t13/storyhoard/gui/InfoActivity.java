890f24901273a0c3690d9a46ec9df01546723de8
Josh Tate <jtate@ualberta.ca>
2013-11-25 17:51:42 -0700

    Linked TextView in InfoActivity

--- src/ca/ualberta/cmput301f13t13/storyhoard/gui/InfoActivity.java
+++ src/ca/ualberta/cmput301f13t13/storyhoard/gui/InfoActivity.java
@@ -1,18 +1,34 @@
 package ca.ualberta.cmput301f13t13.storyhoard.gui;
 
-import ca.ualberta.cmput301f13t13.storyhoard.R;
+import android.app.Activity;
 //import ca.ualberta.cmput301f13t13.storyhoard.R.layout;
 //import ca.ualberta.cmput301f13t13.storyhoard.R.menu;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
+import ca.ualberta.cmput301f13t13.storyhoard.R;
 
 public class InfoActivity extends Activity {
+
+	private TextView infoText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_info);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setUpFields();
+	}
+
+	/**
+	 * Sets up activity fields
+	 */
+	public void setUpFields() {
+		infoText = (TextView) findViewById(R.id.infoText);
 	}
 
 	@Override
