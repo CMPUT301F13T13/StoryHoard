48a6cf34c6cdc760d5065887f390ba994bf41263
Ashley Brown <adbrown@ualberta.ca>
2013-12-01 21:01:27 -0700

    tests for chapter controller done

--- StoryHoardTest/src/ca/ualberta/cmput301f13t13/storyhoard/test/TestChapterController.java
+++ StoryHoardTest/src/ca/ualberta/cmput301f13t13/storyhoard/test/TestChapterController.java
@@ -1,66 +1,106 @@
 package ca.ualberta.cmput301f13t13.storyhoard.test;
-//package ca.ualberta.cmput301f13t13.storyhoard.test;
-//
-//import java.util.ArrayList;
-//import java.util.UUID;
-//
-//import android.content.pm.ActivityInfo;
-//import android.test.ActivityInstrumentationTestCase2;
-//import ca.ualberta.cmput301f13t13.storyhoard.dataClasses.Chapter;
-//import ca.ualberta.cmput301f13t13.storyhoard.dataClasses.Choice;
-//import ca.ualberta.cmput301f13t13.storyhoard.dataClasses.Media;
-//import ca.ualberta.cmput301f13t13.storyhoard.local.BogoPicGen;
-//
-//public class TestChapController extends ActivityInstrumentationTestCase2<ActivityInfo> {
-//
-//	public TestChapController() {
-//		super(ActivityInfo.class);
-//	}
-//
-//	/**
-//	 * Tests adding a choice to a chapter.
-//	 */
-//	public void testAddChoice() {
-//		
-//		Chapter chapter = new Chapter(UUID.randomUUID(), "hello there");
-//		Choice choice = new Choice(chapter.getId(), UUID.randomUUID(), "rawr");
-//		chapter.addChoice(choice);
-//
-//		assertEquals(chapter.getChoices().size(), 1);
-//	}
-//	
-//	/**
-//	 * Tests converting all the bitmaps of the chapter's media into string
-//	 * format, base 64 so they can be placed into the server.
-//	 */
-//	public void testPrepareMedia() {
-//		String path = BogoPicGen.createPath("img1.jpg");
-//		Media photo = new Media(UUID.randomUUID(), path, Media.PHOTO);
-//		Chapter mockChapter = new Chapter(UUID.randomUUID(), "chap texty");
-//		mockChapter.addPhoto(photo);
-//
-//		mockChapter.prepareMediasForServer();
-//		ArrayList<Media> photos = mockChapter.getPhotos();
-//		photo = photos.get(0);
-//
-//		assertFalse(photo.getBitmapString().equals(""));
-//	}	
-//	
-//	/**
-//	 * Tests getting a full chapter back from the database (including choices
-//	 * and media).
-//	 */
-//	public void testGetFullChapter() {
-//		mockChapter = new Chapter(UUID.randomUUID(), "bob went away");
-//		Choice c1 = new Choice(mockChapter.getId(), UUID.randomUUID(), "c1");
-//		Media m1 = new Media(mockChapter.getId(), null, Media.ILLUSTRATION);
-//		choiceCon.insert(c1);
-//		mediaCon.insert(m1);
-//		chapCon.insert(mockChapter);
-//
-//		Chapter newChapter = chapCon.getFullChapter(mockChapter.getId());
-//		assertEquals(newChapter.getChoices().size(), 1);
-//		assertEquals(newChapter.getIllustrations().size(), 1);
-//		assertTrue(mockChapter.getText().equals(newChapter.getText()));
-//	}	
-//}
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import android.test.ActivityInstrumentationTestCase2;
+import ca.ualberta.cmput301f13t13.storyhoard.controllers.ChapterController;
+import ca.ualberta.cmput301f13t13.storyhoard.controllers.StoryController;
+import ca.ualberta.cmput301f13t13.storyhoard.dataClasses.Chapter;
+import ca.ualberta.cmput301f13t13.storyhoard.dataClasses.Choice;
+import ca.ualberta.cmput301f13t13.storyhoard.dataClasses.Media;
+import ca.ualberta.cmput301f13t13.storyhoard.dataClasses.Story;
+import ca.ualberta.cmput301f13t13.storyhoard.helpGuides.InfoActivity;
+import ca.ualberta.cmput301f13t13.storyhoard.local.BogoPicGen;
+import ca.ualberta.cmput301f13t13.storyhoard.local.ChapterManager;
+import ca.ualberta.cmput301f13t13.storyhoard.local.StoryManager;
+import ca.ualberta.cmput301f13t13.storyhoard.local.Utilities;
+
+public class TestChapterController extends ActivityInstrumentationTestCase2<InfoActivity> {
+
+	public TestChapterController() {
+		super(InfoActivity.class);
+	}
+
+	/**
+	 * Tests placing a reference to model in chapter controller so
+	 * it can be manipulated. Also tests that set and get retrieve the same chapter data
+	 * 
+	 */
+	public void testSetCurrChapterCompleteAndIncomplete() {
+		StoryManager sm = StoryManager.getInstance(getActivity());
+		ChapterManager cm = ChapterManager.getInstance(getActivity());
+		ChapterController cc = ChapterController.getInstance(getActivity());
+		Story mockStory = new Story("title1", "author1", "desc1",
+				Utilities.getPhoneId(this.getActivity()));
+		Chapter mockChapter =new Chapter(mockStory.getId(), "new");
+		mockStory.getChapters().add(mockChapter);
+		
+		sm.insert(mockStory);
+		cm.insert(new Chapter(mockStory.getId(), "hello"));
+		cc.setCurrChapterIncomplete(mockChapter.getId());
+		
+
+		cc.setCurrChapterComplete(null);
+		cc.setCurrChapterComplete(mockChapter);
+		
+		assertEquals(cc.getCurrChapter(), mockChapter);
+	}
+	/**
+	 * Tests editing a chapter.
+	 */
+	public void testEditChapter() {
+		StoryManager sm = StoryManager.getInstance(getActivity());
+		ChapterManager cm = ChapterManager.getInstance(getActivity());
+		ChapterController cc = ChapterController.getInstance(getActivity());
+		Story mockStory = new Story("title1", "author1", "desc1",
+				Utilities.getPhoneId(this.getActivity()));
+		Chapter mockChapter =new Chapter(mockStory.getId(), "new");
+		Chapter mockChapter2 =new Chapter(mockStory.getId(), "new2");
+		Choice mockChoice =new Choice(mockChapter.getId(),mockChapter2.getId()); 
+		Choice mockChoice2 =new Choice(mockChoice.getId(),mockChapter.getId(),mockChapter2.getId(),"it worked"); 
+		mockChapter.getChoices().add(mockChoice);
+		mockStory.getChapters().add(mockChapter);
+		
+		sm.insert(mockStory);
+		cm.insert(new Chapter(mockStory.getId(), "hello"));
+		cc.setCurrChapterIncomplete(mockChapter.getId());
+		
+
+		cc.setCurrChapterComplete(null);
+		cc.setCurrChapterComplete(mockChapter);
+		cc.editText("hi");
+		assertEquals(cc.getCurrChapter().getText(), "hi");
+		cc.editRandomChoice(true);
+		assertTrue(cc.getCurrChapter().hasRandomChoice());
+		cc.editRandomChoice(false);
+		assertFalse(cc.getCurrChapter().hasRandomChoice());
+		cc.addChoice(mockChoice);
+		assertEquals(cc.getCurrChapter().getChoices().size(),2);
+		cc.addRandomChoice();
+		assertEquals(cc.getCurrChapter().getChoices().size(),3);
+		cc.updateChoice(mockChoice2);
+		
+		
+		
+	}
+	
+	public void testPushChangesToDb() {
+		StoryManager sm = StoryManager.getInstance(getActivity());
+		ChapterManager cm = ChapterManager.getInstance(getActivity());
+		ChapterController cc = ChapterController.getInstance(getActivity());
+		Story mockStory = new Story("title1", "author1", "desc1",
+				Utilities.getPhoneId(this.getActivity()));
+		Chapter mockChapter =new Chapter(mockStory.getId(), "new");
+		mockStory.getChapters().add(mockChapter);
+		sm.insert(mockStory);
+		cm.insert(new Chapter(mockStory.getId(), "hello"));
+		cc.setCurrChapterIncomplete(mockChapter.getId());
+		
+		cc.setCurrChapterComplete(null);
+		cc.setCurrChapterComplete(mockChapter);
+		cc.pushChangesToDb();
+		Chapter chap = cm.getById(mockChapter.getId());
+		assertNotNull(chap);
+	}
+}
